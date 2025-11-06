24cc420ac363d1caf923aa64e803447de683415e
matheus <matheuscf6@gmail.com>
2025-11-06 17:54:27 -0300

    fix the erros

--- src/pages/tour.tsx
+++ src/pages/tour.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { injectSpeedInsights } from "@vercel/speed-insights";
 
 injectSpeedInsights();
 
 export default function Tour() {
+  const [isVRDevice, setIsVRDevice] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || "";
+    if (/OculusBrowser|Meta Quest|Android/i.test(userAgent)) {
+      setIsVRDevice(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isVRDevice) {
+      window.location.href = "/tour/index.html";
+    }
+  }, [isVRDevice]);
+
+  if (isVRDevice) return null;
+
   return (
     <iframe
       src="/tour/index.html"
